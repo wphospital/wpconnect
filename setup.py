72c7d26765a4c122a1da8249f8352999afa68d70
Sege <jsege@wphospital.org>
2021-05-04 11:32:32 -0400

    try to reference dll path

--- setup.py
+++ setup.py
@@ -1,18 +1,5 @@
 from setuptools import setup
 
-# setup(
-#     name='wpconnect',
-#     url='https://github.com/wphospital/wpconnect',
-#     author='Jon Sege',
-#     author_email='jsege@wphospital.org',
-#     packages=['wpconnect'],
-#     install_requires=['markdown', 'pyodbc', 'pandas', 'cx_Oracle'],
-#     version='0.1',
-#     license='MIT',
-#     description='Internal package for convenience functions in data warehouse',
-#     include_package_data=True,
-# )
-
 def readme():
     with open('README.rst') as f:
         return f.read()
