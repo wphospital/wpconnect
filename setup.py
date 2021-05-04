1d99bc21f143c1ecbdd2e9176c1fb6fe347da852
Sege <jsege@wphospital.org>
2021-05-04 09:51:33 -0400

    move manifest

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='jsege@wphospital.org',
     packages=find_packages(include=['wpconnect']),
     install_requires=['pyodbc', 'pandas', 'cx_Oracle'],
-    version='0.2',
+    version='0.1',
     license='MIT',
     description='Internal package for convenience functions in data warehouse',
     include_package_data=True,
