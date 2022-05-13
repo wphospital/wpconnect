61dbd407271dbe0eb14a2f12fc9ee4bd62560979
Jon Sege <jonsege@gmail.com>
2022-05-13 18:11:43 -0400

    enrich measures and members queries in RPM

--- wpconnect/rpm-queries/measures.sql
+++ wpconnect/rpm-queries/measures.sql
@@ -1,24 +1,36 @@
-select
+with meas AS (
+  select
       m.id as measure_id
-    , md.member_id
-    , md.measure
-    , md.value
-    , md.value_numeric
-    , m.type as base_measure_type
-    , m.measured_at
-    , m.measured_at_tz_offset
-    , m.measure_day_et
-    , m.measure_epoch
-    , mem.first_name
-    , mem.last_name
-    , mem.extern_id
-    , mem.email
-    , mem.first_name || ' ' || mem.last_name as full_name
-    , left(mem.first_name, 1) || left(mem.last_name, 1) as initials
-    , m.updated_at
-    , m.updated_at_tz_offset
-from rpm.measure_details md
-inner join rpm.measures m on m.id = md.measure_id
-left join rpm.members mem on mem.id = md.member_id
-where mem.last_name not like '%%test%%'
-order by member_id, measured_at
+      , md.member_id
+      , md.measure
+      , md.value
+      , md.value_numeric
+      , lag(md.value_numeric) over(partition by md.member_id, md.measure order by m.measured_at) as last_value_numeric
+      , m.type as base_measure_type
+      , m.measured_at
+      , m.measured_at_tz_offset
+      , m.measure_day_et
+      , m.measure_epoch
+      , DATE_PART('day', m.measure_day_et - to_timestamp(bt.enroll_epoch)) as days_since_enrollment
+      , mem.first_name
+      , mem.last_name
+      , mem.extern_id
+      , mem.email
+      , mem.first_name || ' ' || mem.last_name as full_name
+      , left(mem.first_name, 1) || left(mem.last_name, 1) as initials
+      , m.updated_at
+      , m.updated_at_tz_offset
+  from rpm.measure_details md
+  inner join rpm.measures m on m.id = md.measure_id
+  left join rpm.members mem on mem.id = md.member_id
+  left join rpm.billing_tally bt on bt.member_id =  mem.id
+  where mem.last_name not like '%%test%%'
+  order by md.member_id, md.measure, m.measured_at
+)
+
+select
+    meas.*
+  , meas.value_numeric / nullif(meas.last_value_numeric, 0) - 1 as delta_from_last
+from meas
+where 1=1
+  and value_numeric is not null
--- wpconnect/rpm-queries/members.sql
+++ wpconnect/rpm-queries/members.sql
@@ -1,8 +1,10 @@
 select distinct
 	  mem.*
+	, case when mem.extern_id ~ '^\d+$' then lpad(extern_id, 8, '0') else null end as mrn
 	, bt.enroll_epoch
 	, to_timestamp(bt.enroll_epoch) as enroll_datetime
 	, to_timestamp(bt.enroll_epoch)::date as enroll_date
+	, date_trunc('month', to_timestamp(bt.enroll_epoch)::date) as enroll_month
 from rpm.members mem
 inner join rpm.billing_tally bt on bt.member_id = mem.id
 where mem.last_name not like '%%test%%'
