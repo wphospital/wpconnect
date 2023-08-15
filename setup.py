f80c9149dd9dc875e0b5723c10068e57fa09e97e
Jonathan Sege <jsege@wphospital.org>
2023-08-15 09:54:05 -0400

    version control urllib

--- setup.py
+++ setup.py
@@ -34,7 +34,8 @@ setup(
         'plotly',
         'redis',
         'requests',
-        'scipy'
+        'scipy',
+        'urllib3<2'
     ],
     package_data={'wpconnect': ['oracle_dlls/*.dll', 'queries/*.sql', 'rpm-queries/*.sql', 'rpm-cfg.yml']},
     include_package_data=True,
