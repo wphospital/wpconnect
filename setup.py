ef027f96036525ee18d38bafc6ba3fe321319c2d
chfeng99 <118767770+chfeng99@users.noreply.github.com>
2023-11-07 11:13:33 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         'requests',
         'scipy',
         'urllib3<2',
-        'weaviate'
+        'weaviate-client'
     ],
     package_data={'wpconnect': ['oracle_dlls/*.dll', 'queries/*.sql', 'rpm-queries/*.sql', 'rpm-cfg.yml']},
     include_package_data=True,
